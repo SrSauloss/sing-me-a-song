328e9dff4c4214467639ec82b220e33e91f38719
Bruna Topan <brutopanb@gmail.com>
2021-12-07 18:22:21 -0300

    Preparando review

--- src/middlewares/serverMiddlewareErro.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-async function errorMiddleware(err, req, res, next) {
-    console.error('Middleware de erro: ', err);
-    return res.sendStatus(500);
-}
-
-export default errorMiddleware;
